b2f6bfbd2ec9e4b5f7413d4bda6293b7984763db
jrrelucio <jrrelucio@up.edu.ph>
2023-06-07 06:19:08 +0800

    Feat: Icons on Tabs

--- inventory-system/inventorsystemv2.py
+++ inventory-system/inventorsystemv2.py
@@ -1,35 +1,73 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QStackedLayout, QLabel, QComboBox, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit, QDoubleSpinBox
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QStackedLayout, QStyle, QLabel, QComboBox, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit, QDoubleSpinBox, QTextEdit
+from PyQt6.QtGui import QPalette, QColor, QPixmap, QFont
 from layout_colorwidget import Color
+import qtawesome as qta
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-
-        pagelayout = QVBoxLayout()
-        button_layout = QHBoxLayout()
+        self.setWindowTitle("Warehouse Inventory")
+        self.setFixedSize(QSize(700, 700))
+    
+        pagelayout = QHBoxLayout()
+        button_layout = QVBoxLayout()
         self.stacklayout = QStackedLayout()
 
         pagelayout.addLayout(button_layout)
         pagelayout.addLayout(self.stacklayout)
 
-        btn = QPushButton("red")
+        btn_icon = qta.icon('fa5s.box-open')
+        btn = QPushButton(btn_icon, "Inventory")
+        btn.setFixedSize(200, 60)
+        btn.setIconSize(QSize(20, 20))
+        font = QFont("Sans-serif")
+        btn.setFont(font)
         btn.pressed.connect(self.activate_tab_1)
         button_layout.addWidget(btn)
         self.stacklayout.addWidget(Color("red"))
 
-        btn = QPushButton("green")
+        btn_icon = qta.icon('fa5s.sun')
+        btn = QPushButton(btn_icon, "Morning Load")
+        btn.setFixedSize(200, 60)
+        btn.setIconSize(QSize(20, 20))
+        font = QFont("Sans-serif")
+        btn.setFont(font)
         btn.pressed.connect(self.activate_tab_2)
         button_layout.addWidget(btn)
         self.stacklayout.addWidget(Color("green"))
 
-        btn = QPushButton("yellow")
+        btn_icon = qta.icon('fa5s.moon')
+        btn = QPushButton(btn_icon, "Backload")
+        btn.setIconSize(QSize(20, 20))
+        font = QFont("Sans-serif")
+        btn.setFont(font)
+        btn.setFixedSize(200, 60)
         btn.pressed.connect(self.activate_tab_3)
         button_layout.addWidget(btn)
         self.stacklayout.addWidget(Color("yellow"))
+
+        btn_icon = qta.icon('fa5s.clock')
+        btn = QPushButton(btn_icon, "Morning Load History")
+        btn.setIconSize(QSize(20, 20))
+        font = QFont("Sans-serif")
+        btn.setFont(font)
+        btn.setFixedSize(200, 60)
+        btn.pressed.connect(self.activate_tab_4)
+        button_layout.addWidget(btn)
+        self.stacklayout.addWidget(Color("blue"))
+
+        btn_icon = qta.icon('fa5s.clock')
+        btn = QPushButton(btn_icon, "Backload History")
+        btn.setIconSize(QSize(20, 20))
+        font = QFont("Sans-serif")
+        btn.setFont(font)
+        btn.setFixedSize(200, 60)
+        btn.pressed.connect(self.activate_tab_5)
+        button_layout.addWidget(btn)
+        self.stacklayout.addWidget(Color("orange"))
 
         widget = QWidget()
         widget.setLayout(pagelayout)
@@ -44,6 +82,13 @@ class MainWindow(QMainWindow):
     def activate_tab_3(self):
         self.stacklayout.setCurrentIndex(2)
 
+    def activate_tab_4(self):
+        self.stacklayout.setCurrentIndex(3)
+
+    def activate_tab_5(self):
+        self.stacklayout.setCurrentIndex(4)
+
+    
 
 app = QApplication(sys.argv)
 
